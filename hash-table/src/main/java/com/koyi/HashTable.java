8c34bb02e6a4560aa0a128de90496da6954d0a4b
jiangtianhe <jiangtianhe@newrank.cn>
2024-12-05 10:18:30 +0800

    push

--- hash-table/src/main/java/com/koyi/HashTable.java
+++ hash-table/src/main/java/com/koyi/HashTable.java
@@ -1,12 +1,43 @@
 package com.koyi;
 
+import java.util.LinkedList;
+
 public class HashTable {
 
     private int n = 1000;
+    private LinkedList<Student>[] slots = new LinkedList[n];
 
+    public HashTable() {
+        for (int i = 0; i < n; i++) {
+            slots[i] = new LinkedList<>();
+        }
+    }
+
+    public void put(String id, Student student) {
+        int hashValue = hashFuction(id);
+        LinkedList<Student> linkedList = slots[hashValue];
+        linkedList.addFirst(student);
+    }
+
+    public Student get(String id) {
+        int hashValue = hashFuction(id);
+        LinkedList<Student> linkedList = slots[hashValue];
+        for (Student student : linkedList){
+            if (student.id.equals(id)) return student;
+        }
+        return null;
+    }
+
+
+    private int hashFuction(String id) {
+        int idNum = Integer.parseInt(id);
+        int hashValue = idNum % n;
+        return hashValue;
+    }
 }
 
 
-class Student{
-    
+class Student {
+    public String id;
+    public String name;
 }
