c5ae5c29eb3c2500169b8886e9aaf1f2f16e1de9
jiangtianhe <jiangtianhe@newrank.cn>
2024-12-05 10:41:06 +0800

    push

---
+++ hash-table/src/main/java/com/koyi/JavaHashTable.java
@@ -0,0 +1,15 @@
+package com.koyi;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @auther jiangtianhe
+ * @date 2024/12/5
+ */
+public class JavaHashTable {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>(); // 使用HashTable实现
+        HashSet<String> set = new HashSet<>(); // 使用HashTable实现
+    }
+}
